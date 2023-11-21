db867024fbbec3a2c889ac3667ec92e91a64c2cc
Yew Long Leong <yew@cloudflare.com>
2023-11-21 18:39:10 +0800

    add auth

--- src/index.js
+++ src/index.js
@@ -8,10 +8,34 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+
+const ALLOW_LIST = ['panda.png'];
+
+// Check requests for a pre-shared secret
+const hasValidHeader = (request, env) => {
+  return request.headers.get('X-Custom-Auth-Key') === env.AUTH_KEY_SECRET;
+};
+
+function authorizeRequest(request, env, key) {
+  switch (request.method) {
+    case 'PUT':
+    case 'DELETE':
+      return hasValidHeader(request, env);
+    case 'GET':
+      return hasValidHeader(request, env) && ALLOW_LIST.includes(key);
+    default:
+      return false;
+  }
+}
+
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     const key = url.pathname.slice(1);
+
+    if (!authorizeRequest(request, env, key)) {
+      return new Response('Forbidden', { status: 403 });
+    }
 
     switch (request.method) {
       case 'PUT':
